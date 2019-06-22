c87c3718f18316bcc13d735063cfe2d61a6b567b
LucaKleck <LucaKleck@web.de>
2019-06-22 17:28:07 +0200

    Center Text

--- frame/menuPanels/EndScreenPanel.java
+++ frame/menuPanels/EndScreenPanel.java
@@ -5,12 +5,14 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 import core.GameInfo;
 import core.actions.ControlInput;
 import frame.customPresets.JButton_01;
 import frame.customPresets.JPanelCustomBg;
 import frame.graphics.ResourceManager;
+import frame.MainJFrame;
 import frame.customPresets.CustomLable;
 import net.miginfocom.swing.MigLayout;
 
@@ -28,42 +30,42 @@ public class EndScreenPanel extends JPanelCustomBg {
 		add(container, "cell 1 0,grow");
 		
 		if(gameIsWon) {
-			endScreenTitle = "Congratulations! You Won!";
+			endScreenTitle = MainJFrame.makeCssStyle("font-size: 1.15em; font-weight: bold")+"Congratulations! You Won!";
 		} else {
-			endScreenTitle = "You lost! Better luck next time!";
+			endScreenTitle = MainJFrame.makeCssStyle("font-size: 1.15em; font-weight: bold")+"You lost! Better luck next time!";
 		}
 		
-		JLabel endScreenTitleLbl = new CustomLable(endScreenTitle);
+		JLabel endScreenTitleLbl = new CustomLable(endScreenTitle, false, SwingConstants.CENTER);
 		container.add(endScreenTitleLbl, "cell 1 1,grow");
 		
-		JLabel totalWoodLbl = new CustomLable("Total Wood: " + GameInfo.getPlayerStats().getTotalWoodCollected());
+		JLabel totalWoodLbl = new CustomLable("Total Wood: " + GameInfo.getPlayerStats().getTotalWoodCollected(), false, SwingConstants.CENTER);
 		container.add(totalWoodLbl, "cell 1 2,grow");
 		
-		JLabel totalFoodLbl = new CustomLable("Total Food: " + GameInfo.getPlayerStats().getTotalFoodCollected());
+		JLabel totalFoodLbl = new CustomLable("Total Food: " + GameInfo.getPlayerStats().getTotalFoodCollected(), false, SwingConstants.CENTER);
 		container.add(totalFoodLbl, "cell 1 3,grow");
 		
-		JLabel totalStoneLbl = new CustomLable("Total Stone: " + GameInfo.getPlayerStats().getTotalStoneCollected());
+		JLabel totalStoneLbl = new CustomLable("Total Stone: " + GameInfo.getPlayerStats().getTotalStoneCollected(), false, SwingConstants.CENTER);
 		container.add(totalStoneLbl, "cell 1 4,grow");
 		
-		JLabel totalMetalLbl = new CustomLable("Total Metal: " + GameInfo.getPlayerStats().getTotalMetalCollected());
+		JLabel totalMetalLbl = new CustomLable("Total Metal: " + GameInfo.getPlayerStats().getTotalMetalCollected(), false, SwingConstants.CENTER);
 		container.add(totalMetalLbl, "cell 1 5,grow");
 		
-		JLabel totalGoldLbl = new CustomLable("Total Gold: " + GameInfo.getPlayerStats().getTotalGoldCollected());
+		JLabel totalGoldLbl = new CustomLable("Total Gold: " + GameInfo.getPlayerStats().getTotalGoldCollected(), false, SwingConstants.CENTER);
 		container.add(totalGoldLbl, "cell 1 6,grow");
 		
-		JLabel totalManaStoneLbl = new CustomLable("Total ManaStone: " + GameInfo.getPlayerStats().getTotalManaStoneCollected());
+		JLabel totalManaStoneLbl = new CustomLable("Total ManaStone: " + GameInfo.getPlayerStats().getTotalManaStoneCollected(), false, SwingConstants.CENTER);
 		container.add(totalManaStoneLbl, "cell 1 7,grow");
 		
-		JLabel unitsKilled = new CustomLable("Units Killed: " + GameInfo.getPlayerStats().getUnitsKilled());
+		JLabel unitsKilled = new CustomLable("Units Killed: " + GameInfo.getPlayerStats().getUnitsKilled(), false, SwingConstants.CENTER);
 		container.add(unitsKilled, "cell 1 8,grow");
 		
-		JLabel unitsCreated = new CustomLable("Units Created: " + GameInfo.getPlayerStats().getUnitsCreated());
+		JLabel unitsCreated = new CustomLable("Units Created: " + GameInfo.getPlayerStats().getUnitsCreated(), false, SwingConstants.CENTER);
 		container.add(unitsCreated, "cell 1 9,grow");
 		
-		JLabel buildingsDestroyed = new CustomLable("Buildings Destroyed: " + GameInfo.getPlayerStats().getBuildingsDestroyed());
+		JLabel buildingsDestroyed = new CustomLable("Buildings Destroyed: " + GameInfo.getPlayerStats().getBuildingsDestroyed(), false, SwingConstants.CENTER);
 		container.add(buildingsDestroyed, "cell 1 10,grow");
 		
-		JLabel buildingsBuilt = new CustomLable("Buildings Built: " + GameInfo.getPlayerStats().getBuildingsBuilt());
+		JLabel buildingsBuilt = new CustomLable("Buildings Built: " + GameInfo.getPlayerStats().getBuildingsBuilt(), false, SwingConstants.CENTER);
 		container.add(buildingsBuilt, "cell 1 11,grow");
 		
 		JButton btnMainMenu = new JButton_01("Exit to Main Menu");
